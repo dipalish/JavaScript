139e8d8ea9a5e4cea3185815283cf46041d9735f
DIPALI SHARMA <105051716+dipalish@users.noreply.github.com>
2023-10-08 18:14:12 +0530

    Update 02_datatypes.js

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -4,12 +4,14 @@
   // bigint => for larger values
   // String => ""
   // boolean => true || false
-  // null => standalone value
-  // undefined =>
-  // symbol => unique
+  // null => standalone value (iska matlab hai empty or zero nahi hai)
+  // undefined => (aapne variable declare kardiya hai but usmein aap kya value dalenge aapne decide nahi kiya hai)
+  // symbol => unique (kisi bhi value ko unique banane ke liye use kiya jata hai)
 
-  // ************* Non Primitive *************
-  // object
+  // ************* Preference datatype || Non Primitive *************
+  //  1. Array ( [ ] )
+  //  2. Object ( { } )
+  //  3. Function ( function( ){ } )
 
   let age = 3
 

8a1b3c1d56b3a7f6a39daf4a9b678428d9fb97c4
DIPALI SHARMA <105051716+dipalish@users.noreply.github.com>
2023-10-08 09:17:21 +0000

    make changes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+  // **********Primitive ************
+
+  // no. =>  2 to pwr 53 range
+  // bigint => for larger values
+  // String => ""
+  // boolean => true || false
+  // null => standalone value
+  // undefined =>
+  // symbol => unique
+
+  // ************* Non Primitive *************
+  // object
+
+  let age = 3
+
+  console.log(typeof "Dipali")
+  console.log(typeof age)
+
+  // imp - asked in interveiw
+  console.log(typeof null) // object
+  console.log(typeof undefined) // undefined
---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,28 @@
+let score = true
+
+//console.log(typeof score)
+//console.log(typeof (score))
+
+let valueInNumber = Number(score) // Capital No.
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN | type - no.
+// true => 1 || false => 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+// 1 => true | 0 => false
+// "" => false
+// "dipali" => true
+
+let someNo = 33
+let stringNumber = String(someNo)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+  

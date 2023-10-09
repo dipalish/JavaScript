6437a55c4bc2d1077167b3c44c0de2b31605d0c7
DIPALI SHARMA <105051716+dipalish@users.noreply.github.com>
2023-10-09 12:49:42 +0000

    changes

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,17 @@
+function sayMyName(){
+    console.log("D");
+    console.log("I");
+    console.log("P");
+    console.log("A");
+    console.log("L");
+    console.log("I"); 
+}
+
+// sayMyName// refrence
+// sayMyName() // execution
+
+function addTwoNo (num1 , num2){// parameters
+    console.log(num1 +   num2);
+}
+
+addTwoNo(3, null) // arguments

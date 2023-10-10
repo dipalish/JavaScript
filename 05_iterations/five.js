e7bd2d6ca95aa8bb5cc21ce22e4686cf3dfe392c
DIPALI SHARMA <105051716+dipalish@users.noreply.github.com>
2023-10-10 16:02:13 +0000

    make changes

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -9,7 +9,31 @@ coding.forEach( (item) => {
 })
 
 function printMe(item){
-    console.log(item);
+   console.log(item);
 }
 
-coding.forEach(printMe)// only pass the refrence not exceute it
+// coding.forEach(printMe)// only pass the refrence not exceute it
+
+coding.forEach((item , index , arr)=>{
+    // console.log(item, index , arr);
+})
+
+const myCoding = [
+    {
+        languageName : "javascript",
+        languageFileName : "js"
+    },
+    {
+        languageName : "java",
+        languageFileName : "java"
+    },
+    {
+        languageName : "python",
+        languageFileName : "py"
+    },
+    
+]
+
+myCoding.forEach((item)=>{
+    console.log(item.languageFileName);
+})

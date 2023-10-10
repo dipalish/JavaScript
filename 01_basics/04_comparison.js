c5a8480a404de2129147294ab9b8fa7d339b8eaa
DIPALI SHARMA <105051716+dipalish@users.noreply.github.com>
2023-10-10 11:45:28 +0530

    Update 04_comparison.js

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -4,9 +4,13 @@
 console.log(null > 0);
 console.log(null == 0);
 console.log(null >= 0);
+// Here (==) and comparison behaves different . comparison converts null to no. 0 and then compares that's why 1 is F & 3 is T.
 
 console.log(undefined == 0);
 console.log(undefined > 0);
 console.log(undefined > 0);
+// gives F in all scenarios
+
+// NOTE : WE SHOULD AVOID  NULL AND UNDEFINED
 
 // ===   =>  not only it checks value but data type also

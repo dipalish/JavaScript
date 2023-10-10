99b2bd394578af1de0b27753a0710551a62261d2
DIPALI SHARMA <105051716+dipalish@users.noreply.github.com>
2023-10-10 11:37:47 +0530

    Update 04_comparison.js

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -1,5 +1,5 @@
 //console.log("2" > 1)
-// console.log("01" > 1)
+// console.log("01" > 1) || so make sure to compare one which are of same data type coz they might cause some issues sometimes
 
 console.log(null > 0);
 console.log(null == 0);
